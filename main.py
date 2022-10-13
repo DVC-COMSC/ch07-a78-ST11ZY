a91c536da9b18335fdd278e0cb8ea0396a49951c
Your Name <you@example.com>
2022-10-13 16:18:38 -0700

    code

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ words = input('Enter the list: ').split()
 
 are = ['a', 'r', 'e']
 idxlst = []
-
+for i in range (len(words)):
+    if are[1] in words[i] and are[0] in words[i] and are[2] in words[i]:
+        idxlst.append(words[i]);
+print(idxlst)
 # ******************************
 # Make your Code
 # ******************************
